6ccaf5935ed73abefe4fbd9ee899c6d2a1c2d83b
vikkkas <vp030217@gmail.com>
2024-03-11 23:41:25 +0530

    Front end Development completed

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,50 @@
 /** @format */
 
-import React from "react";
+import React, {useState} from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-    return <div > Editor Page < /div>;
+    const [clients,setClients] = useState(
+        [
+            {socketId: 1, username: 'Rakesh k'},
+            {socketId: 2, username: 'John doe'},
+            {socketId: 3, username: 'John doe'}
+
+        ]
+    );
+    return (
+        <div className="mainWrap">
+            {/* Left Pannel */}
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img
+                            className="logoImage"
+                            src="/logo.png"
+                            alt="logo"
+                        />
+                    </div>
+                    
+                    <h3>Connected</h3>
+
+                    <div className="clientLists">
+                        {
+                            clients.map((client)=> (<Client key={client.socketId} username={client.username}/>))
+                        }
+                    </div>
+
+                </div>
+                <button className="btn copyBtn"> Copy Session ID</button>
+                <button className="btn leaveBtn ">Leave Session</button>
+            </div>
+            <div className="editorWrap">
+             <Editor/>
+            </div>
+        </div>
+    );
 };
+
+
 
 export default EditorPage;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,15 @@
 import React, {useState} from "react";
 import { v4 as uuidV4 } from 'uuid';
 import toast from 'react-hot-toast';
+import { useNavigate } from "react-router-dom";
+
 
 
 const Home = () => {
 
     const [sessionId,setSessionId] = useState('');
     const [username,setUsername] = useState('');
+    const navigate = useNavigate();
 
     // Creating unique session ids for each session with help of UUID library.
     const createNewSession = (e) =>{
@@ -17,6 +20,34 @@ const Home = () => {
         setSessionId(id);
         toast.success('Created a new Session')
     }
+
+    // Join Session
+    const joinSession = () =>{
+
+        // Form Validation
+        if (!sessionId || !username){
+            toast.error('Session ID & Username is required')
+            return;
+        }
+
+        // Redirect
+        navigate(`/editor/${sessionId}`, {
+            state: {
+                username,
+            },
+        });
+    
+    };
+
+    // Accessing join button with enter key
+    const handleInputEnter = (e) => {
+        if (e.code === 'Enter') {
+            joinSession();
+        }
+    };
+    
+
+
 return (
     // Logo Section 
         <div className="homePageBox">
@@ -35,7 +66,8 @@ return (
                 placeholder="Session ID"
                 onChange ={(e) => setSessionId(e.target.value)}
                 value={sessionId}
-                />
+                onKeyUp={handleInputEnter}
+                />  
 
     {/*  Username Id Form Section */}
             <input
@@ -44,10 +76,15 @@ return (
                 placeholder="USERNAME"
                 onChange ={(e) => setUsername(e.target.value)}
                 value={username}
+                onKeyUp={handleInputEnter}
                 />
-            <button className="btn joinBtn" >
+                
+    {/* Join Button */}
+            <button className="btn joinBtn" onClick={joinSession} >
                 Join
             </button>
+
+    {/* New session Create section */}
             <span className="createInfo">
                 If you don't have an invite then create &nbsp;
                 <a onClick={createNewSession} href="" className="createNewBtn">
