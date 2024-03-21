7dc422358409f118ac1ae292ba2d85b41656d920
vikkkas <vp030217@gmail.com>
2024-03-21 23:48:13 +0530

    Download Code Button function given to the app.

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -38,7 +38,7 @@ const EditorPage = () => {
                 ({ clients, username, socketId }) => {
                     if (username !== location.state?.username) {
                         toast.success(`${username} joined the session.`);
-                        console.log(`${username} joined`);
+                        console.log(`${username} joined`); 
                     }
                     setClients(clients);
             socketRef.current.emit(ACTIONS.SYNC_CODE, {
@@ -83,8 +83,31 @@ const EditorPage = () => {
     function leaveSession() {
         reactNavigator('/');
     }
+function downloadEditorText() {
+    // Get the text from the codeRef
+    var text = codeRef.current;
 
+    // Create a new Blob object using the text
+    var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
 
+    // Create a link element
+    var link = document.createElement("a");
+
+    // Set the download attribute of the link
+    link.download = "CoCode-"+Date.now()+".txt";
+
+    // Create a URL for the Blob and set it as the href of the link
+    link.href = URL.createObjectURL(blob);
+
+    // Append the link to the body
+    document.body.appendChild(link);
+
+    // Simulate a click on the link
+    link.click();
+
+    // Remove the link from the body
+    document.body.removeChild(link);
+}
     if (!location.state) {
         return <Navigate to="/" />;
     } 
@@ -110,12 +133,14 @@ const EditorPage = () => {
                     </div>
 
                 </div>
+                <button className ="btn downloadBtn" onClick={downloadEditorText}>Download Code</button>
                 <button className="btn copyBtn" onClick={copySessionId}> Copy Session ID</button>
                 <button className="btn leaveBtn" onClick={leaveSession}>Leave Session</button>
             </div>
             <div className="editorWrap">
              <Editor socketRef={socketRef} sessionId={sessionId} onCodeChange = {(code)=>{codeRef.current=code;}}/>
             </div>
+            
         </div>
     );
 };
